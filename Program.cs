654da04f9f485e2fb1519dbacc23edfc229e5beb
TrumanMetz <110424609+TrumanMetz@users.noreply.github.com>
2022-08-04 11:54:41 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{ 
+    public static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int[] intarr = new int[100];
+
+        for (int i = 0; i < intarr.Length; i++)
+        {
+            intarr[i] = rnd.Next();
+            Console.WriteLine(intarr[i]);
+        }
+        Console.WriteLine();
+        int maxNum = intarr.Max();
+        Console.WriteLine("The Max number is " + maxNum);
+        Console.ReadLine();
+    }
+}
