23c150648459ed328975ea8be4aa1f4393c5c7e4
TrumanMetz <110424609+TrumanMetz@users.noreply.github.com>
2022-08-04 15:48:35 -0400

    Update Random program.py

--- Random program.py
+++ Random program.py
@@ -1,5 +1,14 @@
 import random;
+import statistics;
 
-# Make array of 100 random integers
+# Make array of 100 random integers.
 Rarray = [random.randrange(0, 100,1) for _ in range(100)]
+
+# Get wanted values from array.
+maxValue = max(Rarray)
+minValue = min(Rarray)
+meanValue = statistics.mean(Rarray) 
+
+# Print array and values to console. 
 print (Rarray)
+print ("The Maximum value is ", maxValue, "The minimum value is ", minValue, "The Average value is ", meanValue)
