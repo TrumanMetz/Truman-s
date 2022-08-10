1897f9d5a385a6737106696cdb714afc2044b422
Truman J Metz <110424609+TrumanMetz@users.noreply.github.com>
2022-08-10 15:18:44 -0400

    Update Sine Program.cpp

--- Sine Program.cpp
+++ Sine Program.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <fstream>
 using namespace std;
 
 int main()
@@ -13,5 +14,10 @@ int main()
 	for (int i = 0; i <= 360; i = i + 10) {
 		arrayS[i] = sin(i);
 		cout << " , " << arrayS[i];
+	}
+	std::ofstream myfile;
+	myfile.open("Sine program c++.csv");
+	for (int i = 0; i <= 37; i++) {
+		myfile << arrayV[i] << "," << arrayS[i];
 	}
 }
