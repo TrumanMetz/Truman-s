b8173b72766efbeb6ba6421e8df6c9596f01fd62
Truman J Metz <110424609+TrumanMetz@users.noreply.github.com>
2022-08-12 11:33:15 -0400

    Update Sine Program.cpp

--- Sine Program.cpp
+++ Sine Program.cpp
@@ -5,19 +5,22 @@ using namespace std;
 
 int main()
 {
+	// Get values of angles.
 	int arrayV[37];
 	for (int i = 0; i <= 360; i = i + 10) {
 		arrayV[i] = i;
-		cout << " , " << arrayV[i];
+		cout << arrayV[i] << ',';
 	}
+	// Get Sine of the angle.
 	float arrayS[37];
 	for (int i = 0; i <= 360; i = i + 10) {
-		arrayS[i] = sin(i);
-		cout << " , " << arrayS[i];
+		arrayS[i] = sin(i * (3.14159/180));
+		cout << arrayS[i] << ','; 
 	}
 	std::ofstream myfile;
 	myfile.open("Sine program c++.csv");
 	for (int i = 0; i <= 37; i++) {
 		myfile << arrayV[i] << "," << arrayS[i];
 	}
+	myfile.close();
 }
